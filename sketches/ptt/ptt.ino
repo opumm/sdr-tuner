9c137397699ac39505fcf4bcc65a77e8d555c0f1
Mohiuddin Opu <opummjopu@gmail.com>
2021-11-20 11:28:26 +0600

    ptt added

--- sketches/ptt/ptt.ino
+++ sketches/ptt/ptt.ino
@@ -3,54 +3,120 @@
 #define PTT_LED_PIN 5
 #define PTT_BUTTON_PIN 7
 
-byte multiplierLastBtnState = LOW;
+byte multiplierLastBtnState = HIGH;
 byte multiplierLedState = LOW;
-
-unsigned long debounceDuration = 50; // millis
 unsigned long lastTimeButtonStateChanged = 0;
+volatile byte multiplierState = 0;
+
+byte pttBtnState = HIGH;
+byte pttLedState = LOW;
+unsigned long lastTimePttBtnStateChanged = 0;
+
+unsigned long debounceDuration = 70; // millis
+
+static int pinA = 2; // Our first hardware interrupt pin is digital pin 2
+static int pinB = 3; // Our second hardware interrupt pin is digital pin 3
+volatile byte aFlag = 0; // let's us know when we're expecting a rising edge on pinA to signal that the encoder has arrived at a detent
+volatile byte bFlag = 0; // let's us know when we're expecting a rising edge on pinB to signal that the encoder has arrived at a detent (opposite direction to when aFlag is set)
+volatile byte encoderPos = 0; //this variable stores our current value of encoder position. Change to int or uin16_t instead of byte if you want to record a larger range than 0-255
+volatile byte reading = 0; //somewhere to store the direct values we read from our interrupt pins before checking to see if we have moved a whole detent
 
 void setup() {
-  pinMode(LED_PIN, OUTPUT);
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(PTT_LED_PIN, OUTPUT);
+  pinMode(PTT_BUTTON_PIN, INPUT_PULLUP);
+
+  pinMode(MULTIPLIER_LED_PIN, OUTPUT);
+  pinMode(MULTIPLIER_BUTTON_PIN, INPUT_PULLUP);
+
+  pinMode(pinA, INPUT_PULLUP); // set pinA as an input, pulled HIGH to the logic voltage (5V or 3.3V for most cases)
+  pinMode(pinB, INPUT_PULLUP); // set pinB as an input, pulled HIGH to the logic voltage (5V or 3.3V for most cases)
+  attachInterrupt(0,PinA,RISING); // set an interrupt on PinA, looking for a rising edge signal and executing the "PinA" Interrupt Service Routine (below)
+  attachInterrupt(1,PinB,RISING); // set an interrupt on PinB, looking for a rising edge signal and executing the "PinB" Interrupt Service Routine (below)
+
+  Serial.begin(9600);
 }
 
-void loop() {
+void MultiplierBtn(){
   if (millis() - lastTimeButtonStateChanged > debounceDuration) {
-    byte buttonState = digitalRead(BUTTON_PIN);
-    if (buttonState != lastButtonState) {
+    byte buttonState = digitalRead(MULTIPLIER_BUTTON_PIN);
+    if (buttonState != multiplierLastBtnState) {
       lastTimeButtonStateChanged = millis();
-      lastButtonState = buttonState;
+      multiplierLastBtnState = buttonState;
       if (buttonState == LOW) {
-        ledState = (ledState == HIGH) ? LOW: HIGH;
-        digitalWrite(LED_PIN, ledState);
+        multiplierLedState = (multiplierLedState == HIGH) ? LOW: HIGH;
+        digitalWrite(MULTIPLIER_LED_PIN, multiplierLedState);
+        multiplierState = multiplierLedState;
       }
     }
   }
 }
 
+void PttBtn(){
+  if (millis() - lastTimePttBtnStateChanged > debounceDuration) {
+    byte buttonState = digitalRead(PTT_BUTTON_PIN);
+    if (buttonState != pttBtnState) {
+      lastTimePttBtnStateChanged = millis();
+      pttBtnState = buttonState;
+      pttLedState = (pttLedState == HIGH) ? LOW: HIGH;
+      digitalWrite(PTT_LED_PIN, pttLedState);
 
+      if(pttBtnState) {
+        Serial.println(9);
+      } else {
+        Serial.println(2);
+      }
+      delay(20);
+    }
+  }
+}
 
+void PinA(){
+  cli(); //stop interrupts happening before we read pin values
+  reading = PIND & 0xC; // read all eight pin values then strip away all but pinA and pinB's values
+  if(reading == B00001100 && aFlag) { //check that we have both pins at detent (HIGH) and that we are expecting detent on this pin's rising edge
+    encoderPos = 3; //decrement the encoder's position count
+    bFlag = 0; //reset flags for the next turn
+    aFlag = 0; //reset flags for the next turn
+  }
+  else if (reading == B00000100) bFlag = 1; //signal that we're expecting pinB to signal the transition to detent from free rotation
+  sei(); //restart interrupts
+}
 
-// variables will change:
-int buttonState = 0;         // variable for reading the pushbutton status
+void PinB(){
+  cli(); //stop interrupts happening before we read pin values
+  reading = PIND & 0xC; //read all eight pin values then strip away all but pinA and pinB's values
+  if (reading == B00001100 && bFlag) { //check that we have both pins at detent (HIGH) and that we are expecting detent on this pin's rising edge
+    encoderPos = 4; //increment the encoder's position count
+    bFlag = 0; //reset flags for the next turn
+    aFlag = 0; //reset flags for the next turn
+  }
+  else if (reading == B00001000) aFlag = 1; //signal that we're expecting pinA to signal the transition to detent from free rotation
+  sei(); //restart interrupts
+}
 
-void setup() {
-  // initialize the LED pin as an output:
-  pinMode(ledPin, OUTPUT);
-  // initialize the pushbutton pin as an input:
-  pinMode(buttonPin, INPUT_PULLUP);
+void ChangeFreq(){
+  if(encoderPos){
+    if(multiplierState) {
+      Serial.println(encoderPos * 2);
+    } else {
+      Serial.println(encoderPos);
+    }
+    encoderPos = 0;
+  }
 }
 
 void loop() {
-  // read the state of the pushbutton value:
-  buttonState = digitalRead(buttonPin);
+  MultiplierBtn();
+  PttBtn();
 
-  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if (buttonState == LOW) {
-    // turn LED on:
-    digitalWrite(ledPin, HIGH);
-  } else {
-    // turn LED off:
-    digitalWrite(ledPin, LOW);
-  }
+  ChangeFreq();
+  
+//  Serial.print("mlt: ");
+//  Serial.println(multiplierState);
+//
+//  Serial.print("ptt: ");
+//  Serial.println(pttBtnState);
+
+  
+  
 }
--- src/serial.js
+++ src/serial.js
@@ -1,62 +1,88 @@
 document.getElementById('connectTuner').addEventListener('click', () => {
-    if (navigator.serial) {
-      connectSerial();
-    } else {
-      alert('Web Serial API not supported.');
-    }
-  });
-
-  function tryParseJSONObject (jsonString){
-    try {
-        var o = JSON.parse(jsonString);
-        if (o && typeof o === "object") {
-            return o;
-        }
-    }
-    catch (e) { }
-
-    return false;
-  };
-
-  function tuner_freq(value){
-    // var step=les_modes[SDR_RX.mode][5];
-    // SDR_RX.fine = SDR_RX.fine+step*value;
-    // choix_freq_fine();
-    // Affiche_Curseur();
-    const log = document.getElementById('target');
-    log.textContent += 'tune change: ' + value + '\n';
-
+  if (navigator.serial) {
+    connectSerial();
+  } else {
+    alert('Web Serial API not supported.');
   }
-  
-  async function connectSerial() {
-    try {
-      const port = await navigator.serial.requestPort();
-      await port.open({ baudRate: 9600 });
-      
-      const decoder = new TextDecoderStream();
-      
-      port.readable.pipeTo(decoder.writable);
-  
-      const inputStream = decoder.readable;
-      const reader = inputStream.getReader();
-      
-      while (true) {
-        const { value, done } = await reader.read();
-        if (value) {
-          const obj = tryParseJSONObject(value)
-          if (obj){
-            tuner_freq(obj.change)
-          }
-        }
-        if (done) {
-          console.log('[readLoop] DONE', done);
-          reader.releaseLock();
-          break;
-        }
+});
+
+function tuner_freq(value) {
+  // var step=les_modes[SDR_RX.mode][5];
+  // SDR_RX.fine = SDR_RX.fine+step*value;
+  // choix_freq_fine();
+  // Affiche_Curseur();
+
+  console.log('tune change: ' + value + '\n');
+
+}
+
+function ptt_on() {
+  // if(!audioTX.Transmit){
+  //   Transmit_Flip_On_Off();
+  // }
+  console.log('TX started \n');
+}
+
+function ptt_off() {
+  // if(audioTX.Transmit){
+  //   Transmit_Flip_On_Off();
+  // }
+  console.log('TX ended \n');
+}
+
+async function changeState(value){
+  switch (value.trim()) {
+    case '2':
+      ptt_on();
+      break;
+    case '3':
+      tuner_freq(1)
+      break;
+    case '4':
+      tuner_freq(-1)
+      break;
+    case '6':
+      tuner_freq(10)
+      break;
+    case '8':
+      tuner_freq(-10)
+      break;
+    case '9':
+      ptt_off();
+  }
+}
+
+async function connectSerial() {
+  try {
+    const port = await navigator.serial.requestPort();
+    await port.open({
+      baudRate: 9600
+    });
+    console.log('Tuner connected!');
+
+    const decoder = new TextDecoderStream();
+
+    port.readable.pipeTo(decoder.writable);
+
+    const inputStream = decoder.readable;
+    const reader = inputStream.getReader();
+
+    while (true) {
+      const {
+        value,
+        done
+      } = await reader.read();
+      if (value) {
+        changeState(value);
       }
-    
-    } catch (error) {
-      console.log(error);
+      if (done) {
+        console.log('[readLoop] DONE', done);
+        reader.releaseLock();
+        break;
+      }
     }
+
+  } catch (error) {
+    console.log(error);
   }
- 
+}
